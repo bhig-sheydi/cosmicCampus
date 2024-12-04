3485137f9062890bb9150cb153cf7cbb7c08bd4d
bhig-sheydi <bhigsheydi@gmail.com>
2024-12-04 06:13:33 -0800

    changes to deploy adding enhanced student/teachers feature

--- src/components/CustomComponents/TeacherInfoModal.jsx
+++ src/components/CustomComponents/TeacherInfoModal.jsx
@@ -1,4 +1,4 @@
-import { useUser } from '../contexts/UserContext';
+import { useUser } from '@/components/Contexts/userContex';
 import { useEffect } from 'react';
 const TeacherInfoModal = ({ teacher, onClose,  }) => {
 
